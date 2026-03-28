870e5db1ea15e86eb96f32c12b89b289590dfb45
sanae-benkaddour <benkaddoursanae9@gmail.com>
2026-03-28 11:52:32 +0100

    nouvelle branche

--- my_project/main.dart
+++ my_project/main.dart
@@ -30,14 +30,44 @@ Future<void> addProduct(String baseUrl, Map<String, dynamic> product) async {
     print('Erreur lors de l\'ajout du produit');
   }
 }
+Future<void> getOrders(String baseUrl) async {
+  final response = await http.get(Uri.parse('$baseUrl/orders'));
 
+  if (response.statusCode == 200) {
+    List<dynamic> orders = jsonDecode(response.body);
+    print('Commandes disponibles:');
+    for (var order in orders) {
+      print('Produit: ${order['product']}, Quantité: ${order['Quantity']}');
+    }
+  } else {
+    print('Erreur lors de la récupération des commandes');
+  }
+}
+Future<void> addOrder(String baseUrl, Map<String, dynamic> order) async {
+  final response = await http.post(
+    Uri.parse('$baseUrl/orders'),
+    headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+    body: jsonEncode(order),
+  );
+
+  if (response.statusCode == 201) {
+    print('Commande créée avec succès');
+  } else {
+    print('Erreur lors de la création de la commande');
+  }
+}
 void main() async {
   final String baseUrl = 'http://localhost:3000';
 
   print('=== Test du client API Dart ===\n');
 
   // Récupérer tous les produits
-  print('1. Récupération des produits:');
-  await getProducts(baseUrl);
-  print('');
+  // print('1. Récupération des produits:');
+  // await getProducts(baseUrl);
+  // print('');
+
+  // await getOrders(baseUrl);
+
+  final newOrder = {'product': 'PC', 'Quantity': 2};
+  await addOrder(baseUrl, newOrder);
 }
